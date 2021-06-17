0334412a09795b2ad9d444b1b4ccf1ebce8110bc
luna <iszychen@gmail.com>
2021-06-17 19:55:01 +0800

    project init

--- take-out-web/src/main/java/com/luna/meal/MealApplication.java
+++ take-out-web/src/main/java/com/luna/meal/MealApplication.java
@@ -2,12 +2,13 @@ package com.luna.meal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author luna
  * 2021/6/17
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MealApplication {
 
     public static void main(String[] args) {
