c9d2482f732d03c5df2e3e1f0895fb1133e05d40
luna <iszychen@gmail.com>
2021-06-19 20:03:38 +0800

    user face

---
+++ take-out-web/src/main/java/com/luna/meal/rest/FaceRest.java
@@ -0,0 +1,47 @@
+package com.luna.meal.rest;
+
+import com.luna.common.dto.ResultDTO;
+import com.luna.common.dto.constant.ResultCode;
+import com.luna.meal.admin.FaceService;
+import com.luna.meal.admin.LoginService;
+import com.luna.meal.constant.UserConstant;
+import com.luna.meal.util.CookieUtils;
+import com.luna.meal.vo.UserVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author luna@mac
+ * 2021年05月07日 19:02
+ */
+@RestController
+@RequestMapping("/user/api")
+public class FaceRest {
+
+    @Autowired
+    private FaceService faceService;
+
+    @PostMapping("/registerFace")
+    public ResultDTO<Boolean> registerFace(HttpServletRequest request, String faceBase64) {
+        return new ResultDTO<>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS,
+            faceService.registerFace(CookieUtils.getOneSessionKey(request), faceBase64));
+    }
+
+    @PostMapping("/faceLogin")
+    public ResultDTO<UserVO> checkFace(HttpServletResponse response, String faceBase64) {
+        UserVO userVO = faceService.login(faceBase64);
+        Cookie cookie = new Cookie(CookieUtils.SESSION_KEY_NAME, userVO.getSessionKey());
+        cookie.setPath("/");
+        cookie.setMaxAge(UserConstant.SESSION_TIME);
+        response.addCookie(cookie);
+        return new ResultDTO<>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS, userVO);
+    }
+
+}
